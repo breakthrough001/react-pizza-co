19c427edc56554cb4701448e43a666d53141e670
dojo <joe-@dojos-MacBook-Pro.local>
2024-07-31 07:07:31 -0400

    styled menu

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to }) {
-  const className =
-    'inline-block rounded-md bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-500 disabled:hover:bg-yellow-500';
+function Button({ children, disabled, to, type }) {
+  const base =
+    'inline-block rounded-md bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-500 disabled:hover:bg-yellow-500';
+
+  const styles = {
+    primary: base + ' px-4 py-3',
+    small: base + ' px-4 py-2 text-xs',
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
