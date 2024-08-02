dbd0441c39895d6cb3ad1b9931295cd657c68a48
dojo <joe-@dojos-MacBook-Pro.local>
2024-08-02 06:40:56 -0400

    finished styling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,32 +48,42 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* <Form method="POST" action="/order/new"> you don't need to specify the route if you just want it to go to the one you are currently on*/}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col items-start gap-2 sm:items-start">
+          <div className="flex w-full flex-col items-start gap-2 sm:flex-row sm:items-center">
+            <label className="sm:basis-40">Phone number</label>
+            <input className="input grow" type="tel" name="phone" required />
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
+
+          {formErrors?.phone && (
+            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+              {formErrors.phone}
+            </p>
+          )}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
@@ -82,7 +92,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type }) {
   const base =
-    'inline-block rounded-md bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-500 disabled:hover:bg-yellow-500';
+    'inline-block text-sm rounded-md bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-500 disabled:hover:bg-yellow-500';
 
   const styles = {
     primary: base + ' px-4 py-3',
-    small: base + ' px-4 py-2 text-xs',
+    small: base + ' px-4 py-3 text-xs',
+    secondary:
+      'inline-block text-sm rounded-md border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring-2 focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-500 disabled:hover:bg-stone-500 px-4 py-2.5',
   };
 
   if (to)
